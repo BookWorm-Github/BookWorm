f05e8983f0d624c582e88954fb1d60c47ed3063b
Rahul Arasikere <arasikere.rahul@gmail.com>
2019-11-16 14:46:20 -0500

    TODO: fix async call

--- content.js
+++ content.js
@@ -1,3 +1,17 @@
+const deepai = require('deepai');
+deepai.setApiKey('ae158c0c-821b-4319-934e-b8556ee36e39');
+
+function summarize(text){
+  return deepai.callStandardApi("summarization", {
+      text: text
+  }).then(function (result){
+      return result;
+  }).catch(function (error){
+      console.log(error);
+      return error;
+  }); 
+}
+
 function set_phase(int) {
   chrome.storage.local.set({'phase': int}, function () {
     console.log('phase is set to ' + int);

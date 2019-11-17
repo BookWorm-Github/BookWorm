082e84ad16807eca1c4403dac060cd0371145385
Louis Tannudin <ltannudin@gmail.com>
2019-11-16 23:50:00 -0500

    Created an original mode and active mode for popup.

--- popup.js
+++ popup.js
@@ -1,5 +1,14 @@
-document.getElementById('clickme').addEventListener('click', function(tabs) {
+document.getElementById('next-phase').addEventListener('click', function(tabs) {
   chrome.tabs.query({active: true, currentWindow: true}, function (current) {
     chrome.tabs.sendMessage(current[0].id, "next-phase");
+    window.close();
   });
 });
+
+document.getElementById('reset').addEventListener('click',function(tabs){
+  chrome.tabs.query({active:true, currentWindow:true}, function(current){
+    chrome.tabs.sendMessage(current[0].id, "reset");
+    window.close();
+  });
+});
+

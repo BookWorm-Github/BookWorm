e6d04f82a6758aaffd62801e4af90288d54c4273
Louis Tannudin <ltannudin@gmail.com>
2019-11-16 21:10:14 -0500

    updated manifest and tried fixing popup scripts

--- popup.js
+++ popup.js
@@ -1,9 +1,8 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var checkPageButton = document.getElementById('clickIt');
-    checkPageButton.addEventListener('click', function() {
+function hello() {
+  console.log("hi");
+  chrome.tabs.executeScript({
+    file: 'alert.js'
+  });
+}
 
-        chrome.tabs.getSelected(null, function(tab) {
-            alert("You are now on phase x. If you wish you continute, please click on the bookworm logo again!");
-        });
-    }, false);
-}, false);
+document.getElementById('clickme').addEventListener('click', hello);

76a90cadd9e5df01d901894c1bc9cbb88c0e8ba8
Rahul Arasikere <arasikere.rahul@gmail.com>
2020-02-29 14:26:27 -0500

    "added auth quickstart, pending integration"

--- bw_ext/js/background.js
+++ bw_ext/js/background.js
@@ -2,6 +2,24 @@ console.log("background script ran");
 
 // chrome.browserAction.onClicked.addListener(buttonClicked);
 
+var config = {
+    apiKey: 'AIzaSyBkGGKw3_CKSVlsK8XUWRqmjTqcggTmtU0',
+    databaseURL: '<YOUR_DATABASE_URL>',
+    storageBucket: '<YOUR_STORAGE_BUCKET_NAME>'
+  };
+
+function initApp() {
+    // Listen for auth state changes.
+    firebase.auth().onAuthStateChanged(function(user) {
+      console.log('User state change detected from the Background script of the Chrome Extension:', user);
+    });
+  }
+  
+  window.onload = function() {
+    initApp();
+  };
+  
+
 function buttonClicked(tabs) {// 'tab' is an object with information about the current open tab. Increment the phase or resets it back to 0.
     console.log("hello");
     chrome.tabs.sendMessage(tabs.id, 'hello');//send message from background script to content script

4eb6e195d59d739ae4ec34537141a9104b372f26
Sandra Zhen <szhen8930@gmail.com>
2020-03-03 17:31:57 -0500

    attempted chrome extension modified build

--- frontend/portfolio_homepage/public/background.js
+++ frontend/portfolio_homepage/public/background.js
@@ -1,13 +1,55 @@
 chrome.runtime.onInstalled.addListener(() => {
   console.log('onInstalled...');
   // create alarm after extension is installed / upgraded
-  chrome.alarms.create('refresh', { periodInMinutes: 0.1 });
+  scheduleRequest();
+  scheduleWatchdog();
+  startRequest();
 });
 
-chrome.alarms.onAlarm.addListener((alarm) => {
-  console.log(alarm.name); // refresh
-  helloWorld();
+// fetch and save data when chrome restarted, alarm will continue running when chrome is restarted
+chrome.runtime.onStartup.addListener(() => {
+  console.log('onStartup....');
+  startRequest();
 });
+
+// alarm listener
+chrome.alarms.onAlarm.addListener(alarm => {
+  // if watchdog is triggered, check whether refresh alarm is there
+  if (alarm && alarm.name === 'watchdog') {
+    chrome.alarms.get('refresh', alarm => {
+      if (alarm) {
+        console.log('Refresh alarm exists. Yay.');
+      } else {
+        // if it is not there, start a new request and reschedule refresh alarm
+        console.log("Refresh alarm doesn't exist, starting a new one");
+        startRequest();
+        scheduleRequest();
+      }
+    });
+  } else {
+    // if refresh alarm triggered, start a new request
+    startRequest();
+  }
+});
+
+// schedule a new fetch every 30 minutes
+function scheduleRequest() {
+  console.log('schedule refresh alarm to 30 minutes...');
+  chrome.alarms.create('refresh', { periodInMinutes: 30 });
+}
+
+// schedule a watchdog check every 5 minutes
+function scheduleWatchdog() {
+  console.log('schedule watchdog alarm to 5 minutes...');
+  chrome.alarms.create('watchdog', { periodInMinutes: 5 });
+}
+
+// fetch data and save to local storage
+async function startRequest() {
+  console.log('start HTTP Request...');
+  const data = await fetchRepositories();
+  saveToLocalStorage(data);
+}
 
 function helloWorld() {
   console.log("Hello, world!");

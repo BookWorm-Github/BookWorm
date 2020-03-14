5ec1ce5665e0c03ceb2d4b925d3f7f7ccd82977b
Sandra Zhen <szhen8930@gmail.com>
2020-03-14 15:13:54 -0400

    closed window tabs work for current selected window but not for unselected windows

--- frontend/portfolio_homepage/public/background.js
+++ frontend/portfolio_homepage/public/background.js
@@ -2,48 +2,35 @@
 //need to figure out how to get the url of a closed or opened tab in chrome
 window.tabs = [];
 
-var tabsInWindow = {} //tabsInWindow[x] stores all tabs in the window with the windowid 'x'
+var tabsToStoreInLaunch = [];
 
 
 chrome.runtime.onMessage.addListener(
   function(msg, sender, sendResponse) {
       if (msg.rq == "Tabs"){
-        //console.log("Background received request for tabs");
+        console.log("Background received request for tabs");
         getOpenTabs();
         sendResponse({openTabs:window.tabs});
       // window.contentPort = port;
       // port.postMessage({openTabs:window.tabs});
         }
-        else {//console.log("unknown message")
-      }
+        else {console.log("unknown message")}
   });
-
-function getClosedTabs(windowid){//gets closed tabs by comparing tabs that were opened before and tabs currently opened
-    var closedWindowTabs = tabsInWindow[windowid];
-    tabsInWindow[windowid] = [];
-    console.log("getClosedTabs is returning "+closedWindowTabs);
-    return closedWindowTabs;
-}
 
 function getOpenTabs(){//get current open tabs urls
    chrome.tabs.query({currentWindow:true},function(tabs){
     window.tabs.splice(0,window.tabs.length);//clears the window.tabs array
-    if(Array.isArray(tabs) && tabs.length){ //if tabs is not empty
+      console.log("\n/////////////////////\n");
       tabs.forEach(function(tab){
-        console.log("Tab id is "+tab.id);
+        console.log(tab.url);
         window.tabs.push(tab.url);
       });
-      var windowid = tabs[0].windowId;
-      console.log("tab window id is "+windowid)
-      tabsInWindow[windowid] = window.tabs;
-      console.log("Tabs in window "+windowid+" are "+tabsInWindow[windowid].toString())
-    }
-      // //console.log("Length of window tabs is "+window.tabs.length);
+      // console.log("Length of window tabs is "+window.tabs.length);
  });
 }
 
 chrome.tabs.onCreated.addListener(function(windowid) {
- //console.log("tab created");
+ console.log("tab created")
  getOpenTabs();
 
  sendToContent();
@@ -53,9 +40,6 @@ chrome.tabs.onCreated.addListener(function(windowid) {
 
 chrome.windows.onRemoved.addListener(function(windowid) {
  alert("window closed. window id is "+windowid);
- var closedTabsInWindow = getClosedTabs(windowid);
-
- alert("The "+tabs.length+" closed tabs are "+closedTabsInWindow.toString());
 
  // getOpenTabs(); tabs.onRemoved is already updating window tabs
  sendToContent();
@@ -65,28 +49,30 @@ chrome.windows.onRemoved.addListener(function(windowid) {
 
 chrome.tabs.onRemoved.addListener(function(tabid, removed) {
  alert("tab closed: tab id is "+tabid);
+    if(removed.isWindowClosing){
+      tabsToStoreInLaunch.push(tabid);
+    }
     getOpenTabs();
-
     sendToContent();
  // window.contentPort.postMessage({openTabs:window.tabs});
 })
 
 
 chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {//when a newtab is created, get info on how many tabs in current opened window
-    //console.log("tab " + tabId + "updated: " + tab + "\n");
-    //console.log("changeInfo: " + changeInfo);
+    console.log("tab " + tabId + "updated: " + tab + "\n");
+    console.log("changeInfo: " + changeInfo);
     getOpenTabs();
     sendToContent();
 })
 
 chrome.tabs.onActivated.addListener(function(TabInfo) {
-    //console.log("current tab selected is" + TabInfo.tabId + " in window " + TabInfo.windowId);
+    console.log("current tab selected is" + TabInfo.tabId + " in window " + TabInfo.windowId);
     getOpenTabs();
     sendToContent();
 });
 
 function sendToContent(){
-  //console.log("Background is sending to content... ");
+  console.log("Background is sending to content... ");
   chrome.tabs.query({active: true, currentWindow: true},
       tabs =>{
         if(tabs[0]!=undefined)
@@ -104,7 +90,7 @@ function openHomePage(){
 // })
 
     // chrome.runtime.onConnect.addListener(port => {
-    //     //console.log('connected ', port);
+    //     console.log('connected ', port);
 
     //     if (port.name === 'hi') {
     //         port.onMessage.addListener(this.processMessage);
@@ -112,7 +98,7 @@ function openHomePage(){
     // });
 
 // chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
-//   // //console.log("Background received message from "+sender+ " with message "+msg);
+//   // console.log("Background received message from "+sender+ " with message "+msg);
 //   //     switch (msg.type) {
 //   //       case 'popupInit':
 //   //           sendResponse("Sending message from background: "+tabStorage[msg.tabId]);
@@ -131,14 +117,14 @@ function openHomePage(){
 // //send to localhost
 // chrome.tabs.query({url:"*://localhost:*/*"}, function(tabs) {
 //   chrome.tabs.sendMessage(tabs[0].id, {greeting: "hello"}, function(response) {
-//     //console.log("From background to content tab query "+response);
+//     console.log("From background to content tab query "+response);
 //   });
 // });
 
 // chrome.windows.getAll({populate:true}, getAllOpenWindows);
 
 // function getAllOpenWindows(winData) {
-//  //console.log("Getting all opened tabs: ")
+//  console.log("Getting all opened tabs: ")
 //   window.tabs = [];
 //   for (var i in winData) {
 //     if (winData[i].focused === true) {
@@ -149,5 +135,5 @@ function openHomePage(){
 //         }
 //     }
 //   }
-//   //console.log(tabs);
+//   console.log(tabs);
 // }

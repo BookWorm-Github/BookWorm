010df3ab9f257b892a2e181976d043c0041ea97f
Sandra Zhen <szhen8930@gmail.com>
2020-04-02 00:49:28 -0400

    implemented page title grabber

--- frontend/portfolio_homepage/src/urlTitleFetcher/TitleFetcher.js
+++ frontend/portfolio_homepage/src/urlTitleFetcher/TitleFetcher.js
@@ -4,6 +4,7 @@ import React, {Component} from 'react'
 
 import PropTypes from 'prop-types'
 import axios from 'axios'
+import $ from 'jquery'
 //https://urlmeta.org/
 //import $ from 'jquery'
 //import WebTitleFetcher from '../experiments/webtitlefetcher.php'
@@ -20,9 +21,70 @@ class TitleFetcher extends Component{
 
   	}
   	componentDidMount(){
-  		this.getTitles();
+  		// this.getTitles();
+  		var titles = [];
+  		
+
+		function fetchHTML(externalUrl,state,cb){
+			$.ajax({
+			  url: externalUrl,
+			  async: true,
+			  success: function(data) {
+			    var matches = data.match(/<title>(.*?)<\/title>/);
+			    //alert(matches[0]);
+			    titles = [...titles,matches[0]];
+			    	console.log("Titles inside is "+titles.toString());
+			    cb(state,matches[0]);
+			  }   
+			});
+		}
+
+		
+		
+		for(var i = 0; i<this.props.urls.length; i++){
+
+				fetchHTML(this.props.urls[i],this.state.titles,this.callback);
+		}
+
+
+
+  		
+  		console.log("Titles outside is "+this.state.titles.toString());
+
+  		
+
+		// for(var i = 0; i<this.props.urls.length; i++){
+		// 	var html = (await (await fetch(this.props.urls[i])).text()); // html as text
+		// 	var doc = new DOMParser().parseFromString(html, 'text/html');
+		// 	alert("Remote doc title is "+doc.title);
+		// 	if(doc.title!=undefined)
+		// 	this.setState(
+		//       {
+		//         titles: [...this.state.titles, doc.title]
+		//       }
+		//     );
+		//     console.log("State in getHTML is "+this.state.titles.toString());
+			
+
+		// }
   		
   	}
+
+  	callback = (state,t) => {
+			console.log("Param in callback is "+t.toString());
+			//state.push(t);
+			function stripHTMLTags(str) {
+				var s1 = str.replace('<title>', '');
+
+				var s2 = s1.replace('</title>', '');
+				return s2;
+			}
+			var title = stripHTMLTags(t);
+			
+			this.setState({titles: [...this.state.titles,title]})
+			console.log("In callback state is "+this.state.titles.toString());
+
+		}
 	render(){
 
 		return (
@@ -34,37 +96,50 @@ class TitleFetcher extends Component{
 		);
 	}
 
-	getTitles = () =>{
-		console.log("titlefetcher has "+this.props.urls.length+" urls");
-		for(var i = 0; i<this.props.urls.length; i++){
+	// getTitles = () =>{
+	// 	console.log("titlefetcher has "+this.props.urls.length+" urls");
+	// 	for(var i = 0; i<this.props.urls.length; i++){
 			
-			this.getTitle(this.props.urls[i])
+	// 		this.getHTML(this.props.urls[i])
 
-		}
+	// 	}
 
-	}
+	// }
+
+	// getHTML =async  (url) =>{
+	// 	var html = (await (await fetch(url)).text()); // html as text
+	// 	var doc = new DOMParser().parseFromString(html, 'text/html');
+	// 	alert("Remote doc title is "+doc.title);
+	// 	this.setState(
+	//       {
+	//         titles: [...this.state.titles, doc.title]
+	//       }
+	//     );
+	//     console.log("State in getHTML is "+this.state.titles.toString());
+		
+	// }
 
 
-	getTitle=(url)=>{
-		var title = "";
-		var xhr = new XMLHttpRequest();
-		xhr.open("GET", url, true);
-		xhr.onreadystatechange = function() { 
-		  if (xhr.readyState == 4) {
-		    var fetchTitle = (/<title>(.*?)<\/title>/m).exec(xhr.responseText);
-		    if(fetchTitle!=null){ //if the title could be fetched per cors policy
-			    title = fetchTitle[1];
-			    alert(title);
-			    this.setState(
-			      {
-			        titles: [...this.state.titles, title]
-			      }
-			    );
-			}
-		  }
-		}
-		xhr.send();
-	}
+	// getTitle = (url) =>{
+	// 	var title = "";
+	// 	var xhr = new XMLHttpRequest();
+	// 	xhr.open("GET", url, true);
+	// 	xhr.onreadystatechange = function() { 
+	// 	  if (xhr.readyState == 4) {
+	// 	    var fetchTitle = (/<title>(.*?)<\/title>/m).exec(xhr.responseText);
+	// 	    if(fetchTitle!=null){ //if the title could be fetched per cors policy
+	// 		    title = fetchTitle[1];
+	// 		    alert(title);
+	// 		    this.setState(
+	// 		      {
+	// 		        titles: [...this.state.titles, title]
+	// 		      }
+	// 		    );
+	// 		}
+	// 	  }
+	// 	}
+	// 	xhr.send();
+	// }
 
 
 

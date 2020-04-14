ee33e7bd921f9909da0e122a701f6c88deb21dcd
ltann <ltannudin@gmail.com>
2020-04-13 23:01:47 -0400

    function reference on how to store data into database.

--- frontend/portfolio_homepage/src/firebase/firestore/db_functions.js
+++ frontend/portfolio_homepage/src/firebase/firestore/db_functions.js
@@ -2,21 +2,28 @@ import {bw_db} from "../init";
 import React from "react";
 
 export const storeBook = async (book, user) => {
-	const userRef = await bw_db.collection(`users/${user.uid}/bookData`).doc(book.title).get()
-		.then(snapshot => {
-			console.log(snapshot.id)//gets the docs id
-			console.log(snapshot.data())//gets the docs data
+	const bookDataRef = bw_db.collection(`users/${user.uid}/bookData`).doc(book.title)
+	await bookDataRef.get()
+		.then(async snapshot => {//needs to check if the snapshot exists or not
+			if(snapshot.exists){
+				console.log(snapshot.id)//gets the docs id
+				console.log(snapshot.data())//gets the docs data
+			}
+			else{//doc.data() here will be undefined in this case
+				await bookDataRef.set({
+					key: book.key,
+					title: book.title,
+					time_created: book.time_created,
+					Launch: ['testy'],
+					WormHole: {
+						test: 123,
+						string: "possible?"
+					}
+				})
+			}
+		}).catch(e => {
+			console.log("Error getting from document: " + e)
 		})
-	await bw_db.collection(`users/${user.uid}/bookData`).doc(book.title).set({
-		key: book.key,
-		title: book.title,
-		time_created: book.time_created,
-		Launch: ['testy'],
-		WormHole: {
-			test: 123,
-			string: "possible?"
-		}
-	})
 
 	return true
 }

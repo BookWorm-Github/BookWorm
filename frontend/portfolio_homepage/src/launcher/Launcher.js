a1eb93f3d34ffa335393870bb0df7ffc5bc6fcfe
Sandra Zhen <szhen8930@gmail.com>
2020-04-28 01:33:17 -0400

    This does not work Revert "closes prev bw tab upon opening new window"
    This reverts commit 51c814c8f4cbfdc4ed26b59fcb6b890443db5153.

--- frontend/portfolio_homepage/src/launcher/Launcher.js
+++ frontend/portfolio_homepage/src/launcher/Launcher.js
@@ -60,7 +60,6 @@ class Launcher extends Component{
 	_cbWindow = (response) => {
 		//alert("Window "+response.windowId+" was just created");
 		this.props.updateWindow(this.props.book,response.windowId,this.props.book.Launch,this.props.book.WormHole);
-		chrome.runtime.sendMessage({rq: "closeCurrentTab"});
 	}
 
 	
